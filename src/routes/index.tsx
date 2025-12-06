b1a939b30df2192bbb670542975d8737bb64639c
Ryan Luong <lumintriet@gmail.com>
2025-12-06 17:20:27 +0700

    chore(prettier): add tailwind plugin, reconfigure and run formatting.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <div className="h-dvh flex items-center justify-center">
+    <div className="flex h-dvh items-center justify-center">
       <h1>TanStack Start Template</h1>
     </div>
-  )
+  );
 }
