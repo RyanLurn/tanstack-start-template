161f734bfa40e08c3d2fe137e229eb1d7c98eaa3
Ryan Luong <lumintriet@gmail.com>
2025-12-06 17:04:27 +0700

    chore: clean up boilerplate code.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,9 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
-import Header from '../components/Header'
-
-import appCss from '../styles.css?url'
+import appCss from '@/styles.css?url'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -17,7 +15,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'TanStack Start Template',
       },
     ],
     links: [
@@ -38,7 +36,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        <Header />
         {children}
         <TanStackDevtools
           config={{
