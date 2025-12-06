4e7a98f7f0a9730f2bd38a156f8c2035d197e92f
Ryan Luong <lumintriet@gmail.com>
2025-12-06 19:46:33 +0700

    feat: suppress hydration warning on the html tag.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -32,7 +32,7 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <head>
         <HeadContent />
       </head>
